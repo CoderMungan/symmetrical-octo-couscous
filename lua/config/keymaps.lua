d7e0b1cd9c5011edc468ef1783e4e2fd5dfb4d7d
CoderMungan <codermungan@gmail.com>
2024-06-13 14:58:25 +0300

    settings of nvim

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,27 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- Move to line
+vim.api.nvim_set_keymap("n", "<C-u>", "yyddkP", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-d>", "yyddjP", { noremap = true })
+-- New Tab
+vim.api.nvim_set_keymap("n", "te", ":tabedit<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Tab>", ":bn<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<S-Tab>", ":bp<CR>", { noremap = true, silent = true })
+-- Kill Buffer
+vim.api.nvim_set_keymap("n", "<leader>d", ":bd<CR>", { noremap = true, silent = true })
+-- Split Window
+vim.api.nvim_set_keymap("n", "|", ":vsp<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "_", ":sp<CR>", { noremap = true, silent = true })
+-- Resize Window
+vim.api.nvim_set_keymap("n", "<A-Left>", "<C-w><", { noremap = true })
+vim.api.nvim_set_keymap("n", "<A-Right>", "<C-w>>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<A-Up>", "<C-w>+", { noremap = true })
+vim.api.nvim_set_keymap("n", "<A-Down>", "<C-w>-", { noremap = true })
+
+-- Tmux Navigator
+vim.api.nvim_set_keymap("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>", { noremap = true, silent = true })
